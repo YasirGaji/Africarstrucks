effb81c9950a983cdefd9fbceda3391dcc80fae2
Yasir Gaji <gajiyasir@gmail.com>
2022-09-17 07:48:00 +0100

    switching over to next.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
-import './App.css';
+import styled from 'styled-components';
+import tw from 'twin.macro';
+import Homepage from './app/containers/Homepage';
+
+const AppContainer = styled.div`
+  ${tw`
+    w-full
+    h-full
+    flex
+    flex-col
+  `}
+`
 
 function App() {
   return (
-    <div className="App">
-      Hi!
-    </div>
+    <AppContainer>
+      <Homepage />
+    </AppContainer>
   );
 }
 
