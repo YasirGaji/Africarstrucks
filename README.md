f073982765f03b8a3e2cab4f236f2ea68d915e72
Yasir Gaji <gajiyasir@gmail.com>
2022-09-17 07:08:56 +0100

    added styled-component and twind.macro

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 This is a [Africarstrucks](https://#/) created with [`cra-template-redux`](https://github.com/reduxjs/cra-template-redux).
+
+## Libraries
+[Tailwindcss](https://tailwindcss.com/) is used for styling.
+[Styled-components](https://styled-components.com/) is used for styling.
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,17 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html",
   ],
   theme: {
     extend: {},
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    }
   },
   plugins: [],
 }
