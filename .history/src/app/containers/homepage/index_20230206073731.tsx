043545fb5c926415883c7ad136d94d16f9bc78d8
Yasir Gaji <gajiyasir@gmail.com>
2023-02-06 07:39:10 +0100

    working on first component

---
+++ .history/src/app/containers/homepage/index_20230206073731.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components/macro";
+
+const PageContainer = styled.div``;
+
+
+export function HomePage() {
+  return (
+    <PageContainer>
+      <h1>Home Page</h1>
+    </PageContainer>
+  );
+}
