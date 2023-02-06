ea1de5e1a13b500ca3d5368c8a434629be98ef2c
Yasir Gaji <gajiyasir@gmail.com>
2023-02-06 08:03:22 +0100

    rendered the homepage container

--- .history/src/app/containers/homepage/index_20230206074316.tsx
+++ .history/src/app/containers/homepage/index_20230206074316.tsx
@@ -5,11 +5,6 @@ import tw from "twin.macro";
 const PageContainer = styled.div`
   ${tw`
       flex
-      flex-col
-      w-full
-      h-full
-      items-center
-      overflow-hidden
     `}
 `;
 
--- .history/src/app/containers/homepage/index_20230206074420.tsx
+++ .history/src/app/containers/homepage/index_20230206074420.tsx
@@ -2,15 +2,8 @@ import React from "react";
 import styled from "styled-components/macro";
 import tw from "twin.macro";
 
-const PageContainer = styled.div`
-  ${tw`
-      flex
-      flex-col
-      w-full
-      h-full
-      items-center
-      overflow-hidden
-    `}
+const PageContainer = tw.div`
+  
 `;
 
 
